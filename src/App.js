79c5ead69fa32f5c34f5ddaf592932c55f5ce6ac
pranay2508 <bhatnagarpranay8@gmail.com>
2023-07-31 22:25:52 +0530

    .

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React, { useState } from "react";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
-function App() {
+ function App() {
   const [movies, setmovies] = useState([]);
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
+  const [ isLoading , setIsLoading] = useState(false) ; 
+
+  async function fetchMoviesHandler() {
+      setIsLoading(true);
+   const response = await fetch("https://swapi.dev/api/films/");
+   const data = await response.json();
+      
+     
         const transformedMovies = data.results.map( movieData => {
           return {
             id:movieData.episode_id,
@@ -20,7 +22,8 @@ function App() {
           };
         });
         setmovies(transformedMovies);
-      });
+        setIsLoading(false) ; 
+  
   }
 
   return (
@@ -29,7 +32,9 @@ function App() {
         <button onClick = {fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0  && <MoviesList movies={movies} />}
+        { isLoading && movies.length === 0 && <p> No movies found </p>}
+        {isLoading && <p> Loading ... </p>}
       </section>
     </React.Fragment>
   );
